1eafb8b3e0bea4757bf6be1be7527ce69c8d2d56
Ola Lovholm <ola.lovholm@origo.oslo.kommune.no>
2025-04-13 09:36:30 +0200

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Ola Loevholm on 13/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
