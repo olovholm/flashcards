0577160b7d1a95111e40cf5c0b16459ceb4d4c94
Ola Lovholm <ola.lovholm@origo.oslo.kommune.no>
2025-04-13 14:51:30 +0200

    Adds FillBarView and TwoSidedBackgroundView

---
+++ Flashcards/View/FillBarView.swift
@@ -0,0 +1,18 @@
+//
+//  FillBarView.swift
+//  Flashcards
+//
+//  Created by Ola Loevholm on 13/04/2025.
+//
+
+import SwiftUI
+
+struct FillBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FillBarView()
+}
---
+++ Flashcards/View/TwoSidedBackgroundView.swift
@@ -0,0 +1,18 @@
+//
+//  TwoSidedBackgroundView.swift
+//  Flashcards
+//
+//  Created by Ola Loevholm on 13/04/2025.
+//
+
+import SwiftUI
+
+struct TwoSidedBackgroundView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TwoSidedBackgroundView()
+}
